d8ea6c7747570261a2a58122f9de54a21f671820
mersocarlin <mersocarlin@gmail.com>
2015-04-17 23:17:16 -0300

    removing unnecessary comments and code blocks

--- app/components/social-list.js
+++ app/components/social-list.js
@@ -1,9 +1,8 @@
 import React from 'react';
-//var React = require('react');
 
 export default React.createClass({
 
-  /*propTypes: {
+  propTypes: {
     socialList: React.PropTypes.array
   },
 
@@ -11,21 +10,9 @@ export default React.createClass({
     return {
       socialList: []
     };
-  },*/
+  },
 
   render () {
-    /*
-     <ul class="brands brands-inline hidden-xs">
-     <li><a target="_blank" href="https://www.linkedin.com/in/mersocarlin/en"><i class="fa fa-linkedin"></i></a></li>
-     <li><a target="_blank" href="mailto:mersocarlin@mersocarlin.com"><i class="fa fa-envelope-o"></i></a></li>
-     </ul>
-     <h2 class="scrollpoint">mersocarlin</h2>
-     <ul class="brands brands-inline hidden-xs scrollpoint">
-     <li><a target="_blank" href="http://www.facebook.com/mersocarlin"><i class="fa fa-facebook  hi-icon-effect-8"></i></a></li>
-     <li><a target="_blank" href="http://www.twitter.com/mersocarlin"><i class="fa fa-twitter"></i></a></li>
-     </ul>
-     * */
-
     return (
       <div className="social-list">
         <ul className="inline-list">
