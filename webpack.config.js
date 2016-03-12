f6f69eb76c02d60029c4f6ad700309c4541b797c
mersocarlin <mersocarlin@gmail.com>
2016-03-12 15:51:05 -0300

    Add NODE_ENV in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,12 @@ module.exports = {
       $: "jquery",
       jQuery: "jquery",
       "window.jQuery": "jquery"
-    })
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development'),
+      }
+    }),
   ],
 
   module: {
