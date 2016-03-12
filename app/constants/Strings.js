9e6a443afce4dea9ff1e604ea4e55d8f7853d06f
mersocarlin <mersocarlin@gmail.com>
2016-03-12 15:27:10 -0300

    Update gravatar image size

--- app/constants/Strings.js
+++ app/constants/Strings.js
@@ -3,7 +3,7 @@ export default {
 
   App: {
     AppName: 'mersocarlin',
-    GravatarUrl: 'https://s.gravatar.com/avatar/9d345af079c0e2a554a586c6cad3c20c?s=300',
+    GravatarUrl: 'https://s.gravatar.com/avatar/9d345af079c0e2a554a586c6cad3c20c?s=250',
     MyTitle: 'Software Engineer',
   },
 };
