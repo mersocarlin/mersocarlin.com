7b4fc5b145d98393503876d0308572c4255725d3
mersocarlin <mersocarlin@gmail.com>
2017-03-19 16:56:47 +0100

    Integrate react-app-components

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,142 +1,174 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
+import { compose, lifecycle, withHandlers, withState } from 'recompose'
 import classNames from 'classnames'
 import { injectIntl, intlShape } from 'react-intl'
+import { Button, Text, Textarea } from 'react-app-components'
 
 import ErrorMessage from './error-message'
 import FormField from './form-field'
 
-
-class ContactForm extends Component {
-  static propTypes = {
-    error: PropTypes.object,
-    intl: intlShape.isRequired,
-    isSubmiting: PropTypes.bool.isRequired,
-    onSubmit: PropTypes.func.isRequired,
+const FORM_KEYS = ['name', 'email', 'subject', 'message', 'validation']
+const createPayload = () => (FORM_KEYS.reduce((prev, curr) => (
+  {
+    ...prev,
+    [curr]: {
+      isValid: true,
+      value: '',
+    },
   }
+), {}))
 
-  static defaultProps = {
-    error: null,
-  }
+const ContactForm = ({
+  error,
+  intl: { formatMessage },
+  isSubmiting,
+  onFieldChange,
+  onSubmit,
+  payload,
+  isValid,
+}) => {
+  const formCssClass = classNames(
+    'contact-form-component ui form',
+    { error: !isValid || error },
+  )
+  const submitCssClass = classNames(
+    'ui green submit button',
+    { loading: isSubmiting },
+  )
 
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      errorFields: {
-        name: false,
-        email: false,
-        subject: false,
-        message: false,
-        validation: false,
-      },
-      hasError: false,
-    }
-
-    this.handleSubmit = this.handleSubmit.bind(this)
-  }
-
-  handleSubmit () {
-    if (this.props.isSubmiting) return
-
-    const payload = {
-      name: this.name.value,
-      email: this.email.value,
-      subject: this.subject.value,
-      message: this.message.value,
-      validation: this.validation.value,
-    }
-
-    const error = { ...this.state.errorFields }
-
-    Object
-      .keys(error)
-      .forEach(field => (error[field] = !payload[field]))
-
-    if (parseInt(payload.validation, 10) !== 8) {
-      error.validation = true
-    }
-
-    const hasError = Object
-      .keys(error)
-      .filter(field => error[field])
-      .length > 0
-
-    this.setState({ errorFields: error, hasError })
-
-    if (hasError) return
-
-    this.props.onSubmit(payload)
-  }
-
-  render () {
-    const { error, intl: { formatMessage }, isSubmiting } = this.props
-    const { errorFields, hasError } = this.state
-    const formCssClass = classNames(
-      'contact-form-component ui form',
-      { error: hasError || error },
-    )
-    const submitCssClass = classNames(
-      'ui green submit button',
-      { loading: isSubmiting },
-    )
-
-    return (
-      <div className={formCssClass}>
-        <div className="two fields">
-          <FormField
-            label={formatMessage({ id: 'contact.form.name.label' })}
-            hasError={errorFields.name}
-          >
-            <input
-              ref={(name) => { this.name = name }}
-              type="text"
-              placeholder={formatMessage({ id: 'contact.form.name.placeholder' })}
-            />
-          </FormField>
-          <FormField
-            label={formatMessage({ id: 'contact.form.email.label' })}
-            hasError={errorFields.email}
-          >
-            <input
-              ref={(email) => { this.email = email }}
-              type="email"
-              placeholder={formatMessage({ id: 'contact.form.email.placeholder' })}
-            />
-          </FormField>
-        </div>
+  return (
+    <div className={formCssClass}>
+      <div className="two fields">
         <FormField
-          label={formatMessage({ id: 'contact.form.subject.label' })}
-          hasError={errorFields.subject}
+          label={formatMessage({ id: 'contact.form.name.label' })}
+          hasError={!payload.name.isValid}
         >
-          <input
-            ref={(subject) => { this.subject = subject }}
-            type="text"
-            placeholder={formatMessage({ id: 'contact.form.subject.placeholder' })}
+          <Text
+            onChange={value => onFieldChange('name', value)}
+            placeholder={formatMessage({ id: 'contact.form.name.placeholder' })}
+            value={payload.name.value}
           />
         </FormField>
         <FormField
-          label={formatMessage({ id: 'contact.form.message.label' })}
-          hasError={errorFields.message}
+          label={formatMessage({ id: 'contact.form.email.label' })}
+          hasError={!payload.email.isValid}
         >
-          <textarea ref={(message) => { this.message = message }} rows="4" />
-        </FormField>
-        <FormField
-          label={formatMessage({ id: 'contact.form.validation.label' })}
-          hasError={errorFields.validation}
-        >
-          <input
-            ref={(validation) => { this.validation = validation }}
-            type="text"
-            placeholder=""
+          <Text
+            onChange={value => onFieldChange('email', value)}
+            placeholder={formatMessage({ id: 'contact.form.email.placeholder' })}
+            type="email"
+            value={payload.name.email}
           />
         </FormField>
-        { error && <ErrorMessage error={error} /> }
-        <button className={submitCssClass} onClick={this.handleSubmit}>
-          {formatMessage({ id: 'contact.form.send' })}
-        </button>
       </div>
-    )
-  }
+      <FormField
+        label={formatMessage({ id: 'contact.form.subject.label' })}
+        hasError={!payload.subject.isValid}
+      >
+        <Text
+          onChange={value => onFieldChange('subject', value)}
+          placeholder={formatMessage({ id: 'contact.form.subject.placeholder' })}
+          value={payload.name.subject}
+        />
+      </FormField>
+      <FormField
+        label={formatMessage({ id: 'contact.form.message.label' })}
+        hasError={!payload.message.isValid}
+      >
+        <Textarea
+          onChange={value => onFieldChange('message', value)}
+          rows="4"
+          value={payload.name.message}
+        />
+      </FormField>
+      <FormField
+        label={formatMessage({ id: 'contact.form.validation.label' })}
+        hasError={!payload.validation.isValid}
+      >
+        <Text
+          onChange={value => onFieldChange('validation', value)}
+          value={payload.name.validation}
+        />
+      </FormField>
+      { error && <ErrorMessage error={error} /> }
+      <Button className={submitCssClass} onClick={onSubmit}>
+        {formatMessage({ id: 'contact.form.send' })}
+      </Button>
+    </div>
+  )
 }
 
-export default injectIntl(ContactForm)
+ContactForm.propTypes = {
+  error: PropTypes.object,
+  intl: intlShape.isRequired,
+  isSubmiting: PropTypes.bool.isRequired,
+  isValid: PropTypes.bool,
+  onFieldChange: PropTypes.func,
+  onSubmit: PropTypes.func.isRequired,
+  payload: PropTypes.object,
+}
+
+ContactForm.defaultProps = {
+  error: null,
+  isValid: false,
+  onFieldChange: null,
+  payload: null,
+}
+
+export default compose(
+  injectIntl,
+  withState('payload', 'setPayload', createPayload()),
+  withState('isValid', 'toggleValid', true),
+  withHandlers({
+    onFieldChange: ({ payload, setPayload }) => (field, value) => {
+      setPayload({
+        ...payload,
+        [field]: {
+          ...payload[field],
+          value,
+        },
+      })
+    },
+    onSubmit: ({ isSubmiting, onSubmit, payload, setPayload, toggleValid }) => () => {
+      if (isSubmiting) {
+        return
+      }
+
+      const formFields = { ...payload }
+      Object
+        .keys(formFields)
+        .forEach(field => (formFields[field].isValid = !!formFields[field].value))
+
+      if (parseInt(formFields.validation.value, 10) !== 8) {
+        formFields.validation.isValid = false
+      }
+
+      const isValid = Object
+        .keys(formFields)
+        .filter(field => !formFields[field].isValid)
+        .length === 0
+      toggleValid(isValid)
+
+      setPayload({ ...formFields })
+
+      if (!isValid) {
+        return
+      }
+
+      onSubmit(
+        Object
+          .keys(formFields)
+          .filter(key => key !== 'validation')
+          .reduce((prev, curr) => ({
+            ...prev,
+            [curr]: formFields[curr].value,
+          }), {}),
+      )
+    },
+  }),
+  lifecycle({
+    componentDidMount () {
+      this.props.setPayload(createPayload())
+    },
+  }),
+)(ContactForm)
