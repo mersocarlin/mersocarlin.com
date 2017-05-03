58f4dc3209bf32937d4d1c090303fc5a9468390b
mersocarlin <mersocarlin@gmail.com>
2017-05-03 21:27:35 +0200

    Upgrade Home container

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,4 @@
 export Icon from './icon'
+export Image from './image'
 export Menu from './Menu'
+export SocialList from './social-list'
--- src/containers/home.js
+++ src/containers/home.js
@@ -1,73 +1,46 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
+import { compose, lifecycle } from 'recompose'
 import { connect } from 'react-redux'
 
-import { fetchSocialList } from '../actions/social'
-import Icon from '../components/icon'
-import Image from '../components/image'
-import SocialList from '../components/social-list'
+import { fetchSocialList as fetchSocialListAction } from '../actions/social'
+import { Icon, Image, SocialList } from '../components'
+
+import { withIntl } from '../higher-order'
+
 import './home.scss'
 
+const Home = ({ intl, socialList }) => (
+  <div className="page-home">
+    <Image src={intl.formatMessage({ id: 'mersocarlin.gravatarUrl' })} />
+    <h2 className="myName">
+      {intl.formatMessage({ id: 'application.name' })}
+    </h2>
+    <div className="rubberBand animated myTitle">
+      <h3>
+        {intl.formatMessage({ id: 'mersocarlin.title' })}
+        <Icon icon="code" />
+      </h3>
+    </div>
+    {!socialList.isFetching && <SocialList {...socialList} />}
+  </div>
+)
 
-class Home extends Component {
-  static contextTypes = {
-    intl: PropTypes.object,
-  }
-
-  static propTypes = {
-    dispatch: PropTypes.func,
-    socialList: PropTypes.object,
-  }
-
-  static defaultProps = {
-    dispatch: () => { },
-    socialList: {},
-  }
-
-  componentDidMount () {
-    this.props.dispatch(fetchSocialList())
-  }
-
-  renderImage () {
-    return (
-      <Image src={this.context.intl.formatMessage({ id: 'mersocarlin.gravatarUrl' })} />
-    )
-  }
-
-  renderName () {
-    return (
-      <h2 className="myName">
-        {this.context.intl.formatMessage({ id: 'application.name' })}
-      </h2>
-    )
-  }
-
-  renderTitle () {
-    return (
-      <div className="rubberBand animated myTitle">
-        <h3>
-          {this.context.intl.formatMessage({ id: 'mersocarlin.title' })}
-          <Icon icon="code" />
-        </h3>
-      </div>
-    )
-  }
-
-  render () {
-    const { socialList } = this.props
-
-    return (
-      <div className="page-home">
-        {this.renderImage()}
-        {this.renderName()}
-        {this.renderTitle()}
-        {!socialList.isFetching && <SocialList items={socialList.items} />}
-      </div>
-    )
-  }
+Home.propTypes = {
+  intl: PropTypes.object.isRequired,
+  socialList: PropTypes.object.isRequired,
 }
 
-
-export default connect(state => ({
-  socialList: state.socialList,
-}))(Home)
+export default compose(
+  withIntl,
+  connect(state => ({
+    socialList: state.socialList,
+  }), {
+    fetchSocialList: fetchSocialListAction,
+  }),
+  lifecycle({
+    componentDidMount () {
+      this.props.fetchSocialList()
+    },
+  }),
+)(Home)
