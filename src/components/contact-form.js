a3b9fb5a0ee8a37c5db2673c39c06ec239bf10d9
mersocarlin <mersocarlin@gmail.com>
2017-05-03 19:58:25 +0200

    Use prop-types package

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { compose, lifecycle, withHandlers, withState } from 'recompose'
 import classNames from 'classnames'
 import { injectIntl, intlShape } from 'react-intl'
--- src/components/error-message.js
+++ src/components/error-message.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import { injectIntl, intlShape, FormattedMessage } from 'react-intl'
 
 import Icon from './icon'
