1a45809bb555c9e460e3b5973efb50cac32684bc
Hemerson Carlin <mersocarlin@gmail.com>
2017-05-03 20:03:25 +0200

    Feature/prop types (#69)
    * Use prop-types package
    
    * Update webpack config files

--- src/components/map.js
+++ src/components/map.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import GoogleMap from 'google-map-react'
 
 import mapStyle from './map.style'
