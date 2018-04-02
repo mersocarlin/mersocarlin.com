86eb67d2fadf8c55a6919f7bbb552f3a73108384
mersocarlin <mersocarlin@gmail.com>
2018-04-02 16:02:06 +0200

    Fix icon props

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -4,8 +4,9 @@ import { Icon as SemanticIcon } from 'semantic-ui-react'
 
 type ApiPropsT = {
   icon: string,
+  size?: string,
 }
 
-export default function Icon({ icon, ...rest }: ApiPropsT) {
-  return <SemanticIcon name={icon} {...rest} />
+export default function Icon({ icon, size }: ApiPropsT) {
+  return <SemanticIcon name={icon} size={size} />
 }
