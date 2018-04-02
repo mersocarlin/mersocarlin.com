2a51d4e156fc6f553dcd2870515cc837876bba0e
mersocarlin <mersocarlin@gmail.com>
2018-04-02 11:50:12 +0200

    Remove unused files

--- src/config/env.js
+++ src/config/env.js
@@ -1,16 +1,19 @@
 export default {
   environment: process.env.NODE_ENV,
   apiService: {
-    url: process.env.API_SERVICE_URL,
+    url:
+      process.env.API_SERVICE_URL || 'https://mersocarlin-api.herokuapp.com/',
   },
   google: {
-    analyticsId: process.env.GOOGLE_ANALYTICS_ID,
-    mapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+    analyticsId: process.env.GOOGLE_ANALYTICS_ID || 'UA-17163651-1',
+    mapsApiKey:
+      process.env.GOOGLE_MAPS_API_KEY ||
+      'AIzaSyA946EyUxmKjZBttWpQJBIOdOdaBee8LHc',
   },
   map: {
     center: {
-      lat: parseFloat(process.env.MAP_CENTER_LATITUDE),
-      lng: parseFloat(process.env.MAP_CENTER_LONGITUDE),
+      lat: parseFloat(process.env.MAP_CENTER_LATITUDE || '52.5205177'),
+      lng: parseFloat(process.env.MAP_CENTER_LONGITUDE || '13.4014216'),
     },
   },
 }
