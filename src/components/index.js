e3ddd3df80b93d12400ebad411a034f87f926a67
mersocarlin <mersocarlin@gmail.com>
2018-10-06 10:40:49 +0100

    Integrate react-leaflet

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import ContactSent from './ContactSent'
 import Icon from './Icon'
 import Image from './image'
 import Loader from './loader'
-import Map from './map'
+import Map from './Map'
 
 import Menu from './menu'
 import SocialList from './social'
