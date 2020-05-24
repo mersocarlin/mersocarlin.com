4704050bdc4a7ae6e077974a9f1ea2fa80bf7637
mersocarlin <mersocarlin@gmail.com>
2020-05-24 11:59:03 +0100

    Render placeholders

--- src/components/PreviousBlogPosts.tsx
+++ src/components/PreviousBlogPosts.tsx
@@ -1,19 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import { blogPostPlaceholder } from '../utils/constants'
 import Link from './Link'
+import BlogPostCard from './BlogPostCard'
+import BlogPostsGrid from './BlogPostsGrid'
 
 const Main = styled.div`
   font-size: 16rem;
   text-align: right;
 `
 
+const Header = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: var(--padding-xlarge);
+
+  .title {
+    font-size: 20rem;
+    font-weight: bold;
+  }
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+  }
+`
+
 export default function PreviousBlogPosts() {
   return (
     <Main>
-      <Link as="/blog" href="/blog">
-        View all blog posts ➡️
-      </Link>
+      <Header>
+        <div className="title">Previous Blog Posts</div>
+        <Link as="/blog" href="/blog">
+          View all blog posts ➡️
+        </Link>
+      </Header>
+
+      <BlogPostsGrid>
+        <BlogPostCard disabled post={blogPostPlaceholder} />
+        <BlogPostCard disabled post={blogPostPlaceholder} />
+        <BlogPostCard disabled post={blogPostPlaceholder} />
+      </BlogPostsGrid>
     </Main>
   )
 }
