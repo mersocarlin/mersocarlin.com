c8a6cc3781032211a0ed9acb20f70ba89b5d0272
mersocarlin <mersocarlin@gmail.com>
2021-01-03 13:56:53 +0000

    Use default dark theme from tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   purge: ['./src/components/**/*.tsx', './src/pages/**/*.tsx'],
   darkMode: 'class',
   theme: {
     extend: {},
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.trueGray,
-      red: colors.red,
-    },
     fontFamily: {
       sans: ['Josefin Sans', 'Helvetica', 'Arial'],
     },
