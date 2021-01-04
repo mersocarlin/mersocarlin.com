07a60e241ec133c5842cd0f4773cf73de3a48ce5
mersocarlin <mersocarlin@gmail.com>
2021-01-04 21:01:15 +0000

    Add structured data to blog post cards

--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -7,24 +7,23 @@ import BlogPostDate from './BlogPostDate'
 import Link from './Link'
 
 interface BlogPostCardProps {
-  disabled?: boolean
   post: Post
 }
 
-export default function BlogPostCard({ disabled, post }: BlogPostCardProps) {
+export default function BlogPostCard({ post }: BlogPostCardProps) {
   const blogContent = (
-    <div
-      className={`group rounded shadow-md cursor-pointer hover:shadow-lg overflow-hidden mersocarlin-bg-white mersocarlin-text-gray ${
-        Boolean(disabled) ? 'cursor-default' : 'cursor-pointer'
-      }`}
-    >
+    <div className="group rounded shadow-md cursor-pointer hover:shadow-lg overflow-hidden mersocarlin-bg-white mersocarlin-text-gray">
       <Image src={post.images.coverUrl} height={500} width={1000} />
       <div className="flex justify-center flex-col justify-between p-3 h-48">
-        <div className="text-center mb-2 text-xl font-bold group-hover:underline">
+        <div
+          className="text-center mb-2 text-xl font-bold group-hover:underline"
+          itemProp="name"
+        >
           {post.title}
         </div>
         <p
           className="overflow-ellipsis overflow-hidden my-2 text-sm line-clamp-3"
+          itemProp="name"
           title={post.excerpt}
         >
           {post.excerpt}
@@ -39,10 +38,6 @@ export default function BlogPostCard({ disabled, post }: BlogPostCardProps) {
       </div>
     </div>
   )
-
-  if (disabled) {
-    return blogContent
-  }
 
   return (
     <Link
--- src/components/BlogPostsGrid.tsx
+++ src/components/BlogPostsGrid.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 function BlogPostsGrid({ children }: Props) {
   return (
-    <div className="px-4 md:px-0 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
+    <ul className="px-4 md:px-0 grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
       {children}
-    </div>
+    </ul>
   )
 }
 
