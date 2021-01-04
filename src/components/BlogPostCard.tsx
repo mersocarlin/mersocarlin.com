f4aa336cea1168daa6eb4bb75f4173f0caac758d
mersocarlin <mersocarlin@gmail.com>
2021-01-04 21:39:16 +0000

    Cleanup BlogPostCard

--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -11,41 +11,37 @@ interface BlogPostCardProps {
 }
 
 export default function BlogPostCard({ post }: BlogPostCardProps) {
-  const blogContent = (
-    <div className="group rounded shadow-md cursor-pointer hover:shadow-lg overflow-hidden mersocarlin-bg-white mersocarlin-text-gray">
-      <Image src={post.images.coverUrl} height={500} width={1000} />
-      <div className="flex justify-center flex-col justify-between p-3 h-48">
-        <div
-          className="text-center mb-2 text-xl font-bold group-hover:underline"
-          itemProp="name"
-        >
-          {post.title}
-        </div>
-        <p
-          className="overflow-ellipsis overflow-hidden my-2 text-sm line-clamp-3"
-          itemProp="name"
-          title={post.excerpt}
-        >
-          {post.excerpt}
-        </p>
-
-        <div className="flex justify-between text-sm mersocarlin-text-gray font-light">
-          <span>{post.author.name}</span>
-          <span>
-            <BlogPostDate post={post} />
-          </span>
-        </div>
-      </div>
-    </div>
-  )
-
   return (
     <Link
       as={`/blog/${post.slug}`}
       hoverStyles="hover:no-underline"
       href="/blog/[slug]"
     >
-      {blogContent}
+      <div className="group rounded shadow-md cursor-pointer hover:shadow-lg overflow-hidden mersocarlin-bg-white mersocarlin-text-gray">
+        <Image src={post.images.coverUrl} height={500} width={1000} />
+        <div className="flex justify-center flex-col justify-between p-3 h-48">
+          <div
+            className="text-center mb-2 text-xl font-bold group-hover:underline"
+            itemProp="name"
+          >
+            {post.title}
+          </div>
+          <p
+            className="overflow-ellipsis overflow-hidden my-2 text-sm line-clamp-3"
+            itemProp="name"
+            title={post.excerpt}
+          >
+            {post.excerpt}
+          </p>
+
+          <div className="flex justify-between text-sm mersocarlin-text-gray font-light">
+            <span>{post.author.name}</span>
+            <span>
+              <BlogPostDate post={post} />
+            </span>
+          </div>
+        </div>
+      </div>
     </Link>
   )
 }
