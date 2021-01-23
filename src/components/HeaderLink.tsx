1d02b4a47af214219b7b26feba735d9c977cc7eb
Hemerson Carlin <hcarlin@hubspot.com>
2021-01-23 22:41:37 +0000

    Extract HeaderLink component

---
+++ src/components/HeaderLink.tsx
@@ -0,0 +1,33 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+import Link from './Link'
+
+type Props = {
+  children: React.ReactNode
+  fontStyles?: string
+  path: string
+}
+
+function HeaderLink({
+  children,
+  fontStyles = 'text-sm md:text-base',
+  path,
+}: Props) {
+  const router = useRouter()
+
+  // do not highlight home
+  const isActive = path !== '/' && router.pathname === path
+
+  return (
+    <Link
+      className={isActive ? 'underline' : ''}
+      fontStyles={fontStyles}
+      colorStyles="mersocarlin-text-gray hover:text-gray-600 visited:text-gray-600 dark:hover:text-gray-100 dark:visited:text-gray-100"
+      href={path}
+    >
+      {children}
+    </Link>
+  )
+}
+
+export default HeaderLink
