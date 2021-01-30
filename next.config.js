1b700c0972b410af4307d45ae9c28fda09802668
Hemerson Carlin <hcarlin@hubspot.com>
2021-01-30 13:53:42 +0000

    Create sitemap

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   images: {
     domains: ['pbs.twimg.com'],
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./scripts/create-sitemap')
+    }
+
+    return config
+  },
 }
