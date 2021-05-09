07c7362a25f167619d13e507241004c8564aedd5
Hemerson Carlin <hcarlin@hubspot.com>
2021-05-09 22:15:35 +0100

    Update blog post search

--- src/modules/blog/utils/searchBlogPosts.spec.ts
+++ src/modules/blog/utils/searchBlogPosts.spec.ts
@@ -15,6 +15,7 @@ const atomicMoneyPost: Post = {
   date: new Date().toISOString(),
   excerpt: '',
   slug: 'atomic-money-from-a-spreadsheet-to-a-side-project',
+  tags: ['javascript', 'typescript'],
   title: 'Atomic Money - from a spreadsheet to a side project',
   type: 'preview',
 }
@@ -32,6 +33,7 @@ const uniqueIdPost: Post = {
   date: new Date().toISOString(),
   excerpt: '',
   slug: 'code-snippets-uniqueid',
+  tags: ['docker', 'nodejs'],
   title: 'Code Snippets - uniqueId',
   type: 'preview',
 }
@@ -45,7 +47,7 @@ describe('searchBlogPosts', () => {
     expect(result).toEqual(posts)
   })
 
-  describe('text search', () => {
+  describe('query search', () => {
     it('should filter blogposts by query search', () => {
       const result = searchBlogPosts(posts, { query: 'money' })
 
@@ -57,5 +59,28 @@ describe('searchBlogPosts', () => {
 
       expect(result).toEqual([])
     })
+  })
+
+  describe('tag search', () => {
+    it('should filter blogposts by tag', () => {
+      const result = searchBlogPosts(posts, { tag: 'javascript' })
+
+      expect(result).toEqual([atomicMoneyPost])
+    })
+
+    it('should return no results', () => {
+      const result = searchBlogPosts(posts, { tag: 'leadership' })
+
+      expect(result).toEqual([])
+    })
+  })
+
+  it('should filter posts by query and tag', () => {
+    const result = searchBlogPosts(posts, {
+      query: 'money',
+      tag: 'javascript',
+    })
+
+    expect(result).toEqual([atomicMoneyPost])
   })
 })
--- src/modules/blog/utils/searchBlogPosts.ts
+++ src/modules/blog/utils/searchBlogPosts.ts
@@ -1,17 +1,34 @@
-import { Post } from '@mersocarlin.com/types'
+import { Post, TagT } from '@mersocarlin.com/types'
 
 interface SearchOptions {
   query?: string
+  tag?: TagT
+}
+
+function filterByQuery(query?: string) {
+  const regSearch = new RegExp(query || '', 'i')
+
+  return (post: Post) => {
+    if (!query) {
+      return true
+    }
+
+    return regSearch.test(post.title)
+  }
+}
+
+function filterByTag(tag?: TagT) {
+  return (post: Post) => {
+    if (!tag) {
+      return true
+    }
+
+    return post.tags.includes(tag)
+  }
 }
 
 export default function searchBlogPosts(posts: Post[], options: SearchOptions) {
-  if (!options.query) {
-    return posts
-  }
-
-  const regSearch = new RegExp(options.query, 'i')
-
-  return posts.filter((post) => {
-    return regSearch.test(post.title)
-  })
+  return posts
+    .filter(filterByQuery(options.query))
+    .filter(filterByTag(options.tag))
 }
