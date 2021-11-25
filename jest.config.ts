90d14c1a2090e7f8ef6d43bae5059fc36caf6987
Hemerson Carlin <hcarlin@hubspot.com>
2021-11-25 19:38:58 +0000

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,10 @@
-export default {
+import nextJest from 'next/jest'
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: '.' })
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
@@ -9,3 +15,8 @@ export default {
     '@twitter/(.*)': '<rootDir>/src/modules/twitter/$1',
   },
 }
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+const jestConfig = createJestConfig(customJestConfig)
+
+export default jestConfig
