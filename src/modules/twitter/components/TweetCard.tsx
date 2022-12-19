4191b03ec71d729687473761d2130970c40f9d2a
Hemerson Carlin <mersocarlin@gmail.com>
2022-12-19 16:01:32 +0000

    Use new next image

--- src/modules/twitter/components/TweetCard.tsx
+++ src/modules/twitter/components/TweetCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Image from 'next/legacy/image'
+import Image from 'next/image'
 
 import { Tweet } from '@mersocarlin.com/types'
 import Link from '@common/components/Link'
