718188b9a9acc06b45e73fa5370523ea23d9ee90
Hemerson Carlin <mersocarlin@gmail.com>
2022-12-19 15:52:05 +0000

    Update image props

--- src/modules/blog/components/BlogPostImage.tsx
+++ src/modules/blog/components/BlogPostImage.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import Image from 'next/legacy/image'
 
-interface Props {
-  alt: string
-  height: string
-  priority?: boolean
-  src: string
-  title: string
-  width: string
-}
-
 export default function BlogPostImage({
   alt,
   height,
@@ -17,17 +8,24 @@ export default function BlogPostImage({
   src,
   title,
   width,
-}: Props) {
+}: {
+  alt: string
+  height: number
+  priority?: boolean
+  src: string
+  title: string
+  width: number
+}) {
   return (
     <div className="my-8 text-center">
       <Image
         alt={alt}
         className="rounded-md shadow-md mb-2"
-        height={height}
+        height={`${height}`}
         priority={priority}
         src={src}
         title={title}
-        width={width}
+        width={`${width}`}
       />
       <p className="text-sm">{title}</p>
     </div>
