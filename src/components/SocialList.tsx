6896b6efdfa3e5ab929f11df5a13006096ea8786
Hemerson Carlin <mersocarlin@gmail.com>
2022-12-19 15:32:00 +0000

    Next image

--- src/components/SocialList.tsx
+++ src/components/SocialList.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'next/image'
+import Image from 'next/legacy/image'
 
 import Link from '@common/components/Link'
 import { socialListItems } from '@mersocarlin.com/utils/social'
