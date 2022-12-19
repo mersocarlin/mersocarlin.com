fe528fbc041c8ded1947b8422822c7d215ad0faa
Hemerson Carlin <mersocarlin@gmail.com>
2022-12-19 15:45:07 +0000

    Integrate @next/font

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,31 +34,6 @@ export default function MyDocument() {
 
         <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
 
-        {/* Load Google fonts asynchronously */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              WebFontConfig = {
-                google: {
-                  families: [ 'Josefin+Sans:300,400,600,700&display=swap']
-                }
-              };
-  
-              (function() {
-                var wf = document.createElement('script');
-                wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-                  '://ajax.googleapis.com/ajax/libs/webfont/1.5.18/webfont.js';
-                wf.type = 'text/javascript';
-                wf.async = 'true';
-                var s = document.getElementsByTagName('script')[0];
-                s.parentNode.insertBefore(wf, s);
-              })();
-              `,
-          }}
-          key="josefinsans"
-          type="text/javascript"
-        />
-
         <style
           dangerouslySetInnerHTML={{
             __html: `
