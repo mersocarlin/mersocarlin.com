17185f808586d25ec3ff7f81df8de6a1af762cdf
Hemerson Carlin <mersocarlin@gmail.com>
2023-01-17 21:54:06 +0000

    Update index and  pages

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,33 +1,76 @@
-export default function Index() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
-      <h1>Welcome to Remix</h1>
+import { useLoaderData } from '@remix-run/react'
+import AppLink from '~/components/AppLink'
+import Avatar from '~/components/Avatar'
+import BlogPostsGrid from '~/components/BlogPostsGrid'
+import BlogPostCard from '~/components/BlogPostCard'
+import { getLatestBlogPosts } from '~/utils/post.server'
 
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+export async function loader() {
+  const posts = await getLatestBlogPosts()
+
+  return { posts }
+}
+
+export default function Index() {
+  const { posts } = useLoaderData<typeof loader>()
+
+  return (
+    <div className="flex flex-col mersocarlin-text-gray px-8 md:p-0">
+      <section>
+        <Avatar size={150} />
+
+        <h1 className="text-center text-3xl font-bold pt-4">Hemerson Carlin</h1>
+        <h2 className="text-center text-lg">Tech Lead / Software Engineer</h2>
+      </section>
+
+      <section className="flex flex-col space-y-4 mt-12">
+        <p className="text-lg">Hello there 👋🏼</p>
+        <p className="text-lg">
+          {`I'm Hemerson Carlin, also know as `}
+          <em>mersocarlin</em>, a passionate and resourceful full-stack Software
+          Engineer with 10+ years of experience focused on agile development,
+          architecture and team building.
+        </p>
+        <p className="text-lg">
+          I have experience in designing and developing web applications using
+          microservices architecture alongside with JavaScript, Node.js and
+          React.
+        </p>
+        <p className="text-lg">
+          {`I'm Technical Lead at `}
+          <AppLink href="https://www.hubspot.com/" target="_blank">
+            HubSpot
+          </AppLink>{' '}
+          and based in Dublin, Ireland.
+        </p>
+        <p className="text-lg">
+          I also created{' '}
+          <AppLink href="https://atomicmoney.app" target="_blank">
+            Atomic Money
+          </AppLink>
+          : an expense tracker completely free for you to monitor your income
+          and expenses.
+        </p>
+      </section>
+
+      <section className="mt-12">
+        <h3 className="mersocarlin-text-gray font-bold text-xl mb-4">
+          Featured blog posts
+        </h3>
+
+        <BlogPostsGrid>
+          {posts.map((post) => (
+            <li
+              itemProp="blogPost"
+              itemScope={true}
+              itemType="https://schema.org/BlogPosting"
+              key={post.slug}
+            >
+              <BlogPostCard post={post} />
+            </li>
+          ))}
+        </BlogPostsGrid>
+      </section>
     </div>
   )
 }
