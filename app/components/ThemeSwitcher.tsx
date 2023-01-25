2c417624a79c38b7112a84301deacb4e56395224
Hemerson Carlin <mersocarlin@gmail.com>
2023-01-25 21:46:41 +0000

    Social icons (#294)
    * Extract icon components
    
    * Update SocialList
    
    * Update image styles
    
    * Remove svg files
    
    * Sort social items

--- app/components/ThemeSwitcher.tsx
+++ app/components/ThemeSwitcher.tsx
@@ -1,49 +1,11 @@
 import { useEffect, useState } from 'react'
 import { useTheme } from '~/providers/ThemeProvider'
-
-function Sun() {
-  return (
-    <svg
-      className="w-4 h-4 mersocarlin-text-gray mx-auto"
-      fill="currentColor"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle cx="12" cy="12" r="5" />
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2.4}
-        d="M12 1v2m0 18v2M4.22 4.22l1.42 1.42m12.72 12.72l1.42 1.42M1 12h2m18 0h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42"
-      />
-    </svg>
-  )
-}
-
-function Moon() {
-  return (
-    <svg
-      className="w-4 h-4 mersocarlin-text-gray mx-auto"
-      fill="currentColor"
-      stroke="currentColor"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth={2}
-        d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"
-      />
-    </svg>
-  )
-}
+import { MoonIcon, SunIcon } from './Icons'
 
 function ThemeIcon() {
   const [theme] = useTheme()
 
-  return theme === 'dark' ? <Sun /> : <Moon />
+  return theme === 'dark' ? <SunIcon /> : <MoonIcon />
 }
 
 export default function ThemeSwitcher() {
--- app/hooks/useMdxComponent.tsx
+++ app/hooks/useMdxComponent.tsx
@@ -8,7 +8,7 @@ const mdxComponents = {
 function PostImage(props: JSX.IntrinsicElements['img']) {
   return (
     <figure>
-      <img alt="" className="w-full rounded-lg object-cover" {...props} />
+      <img alt="" className="mx-auto rounded-lg object-cover" {...props} />
       <figcaption className="text-center mersocarlin-text-gray">
         {props.alt}
       </figcaption>
