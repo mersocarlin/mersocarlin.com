ca59bee01c573431c10f6677b3897d5e16043d08
Hemerson Carlin <mersocarlin@gmail.com>
2023-01-29 22:52:48 +0000

    Update blog post card styles

--- app/components/BlogPostCard.tsx
+++ app/components/BlogPostCard.tsx
@@ -5,15 +5,17 @@ import BlogPostDate from './BlogPostDate'
 export default function BlogPostCard({ post }: { post: Post }) {
   return (
     <AppLink hoverStyles="hover:no-underline" href={`/blog/${post.slug}`}>
-      <div className="group rounded shadow-md cursor-pointer hover:shadow-lg overflow-hidden mersocarlin-bg-white mersocarlin-text-gray">
-        <img
-          alt={post.title}
-          height={post.coverImage.height}
-          itemProp="image"
-          src={post.coverImage.url}
-          width={post.coverImage.width}
-        />
-        <div className="flex flex-col justify-between p-3 h-48">
+      <div className="group rounded shadow-md cursor-pointer hover:shadow-lg overflow-hidden mersocarlin-bg-white mersocarlin-text-gray min-h-[425px] flex flex-col">
+        <div>
+          <img
+            alt={post.title}
+            height={post.coverImage.height}
+            itemProp="image"
+            src={post.coverImage.url}
+            width={post.coverImage.width}
+          />
+        </div>
+        <div className="flex flex-col justify-between p-3 flex-1">
           <div
             className="text-center text-xl font-bold group-hover:underline"
             itemProp="headline"
@@ -21,14 +23,14 @@ export default function BlogPostCard({ post }: { post: Post }) {
             {post.title}
           </div>
           <p
-            className="text-ellipsis overflow-hidden text-sm line-clamp-3"
+            className="text-ellipsis overflow-hidden line-clamp-3 text-[16px] max-h-[300px] my-5"
             itemProp="description"
             title={post.excerpt}
           >
             {post.excerpt}
           </p>
 
-          <div className="flex justify-between text-md mersocarlin-text-gray font-light">
+          <div className="flex justify-between mersocarlin-text-gray font-light">
             <div
               itemProp="author"
               itemScope={true}
